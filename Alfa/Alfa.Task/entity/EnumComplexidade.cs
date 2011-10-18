7fc84f6af50af4fec1741a880b1914fbfdccd8ad
fabioelias77 <fabioelias77@gmail.com>
2011-10-18 21:13:00 +0000

    inclusão de classes no aplicativo

---
+++ Alfa/Alfa.Task/entity/EnumComplexidade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Alfa.Task.Enum
+{
+    public enum EnumComplexidade
+    {
+        Baixa = 1,
+        Media,
+        Alta
+    }
+}
